f894c0e1106139ffb28bd6b6cd3ff9384d155ef0
Ben Lacey <85163278+benlacey57@users.noreply.github.com>
2024-08-01 09:25:30 +0000

    Committing progress

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,31 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ComponentsProductAttributes extends Schema.Component {
+  collectionName: 'components_components_product_attributes';
+  info: {
+    displayName: 'productAttributes';
+    description: '';
+  };
+  attributes: {
+    name: Attribute.String & Attribute.Required;
+    value: Attribute.String & Attribute.Required;
+    description: Attribute.Text & Attribute.Required;
+  };
+}
+
+export interface ComponentsProductFeature extends Schema.Component {
+  collectionName: 'components_components_product_features';
+  info: {
+    displayName: 'productFeature';
+    description: '';
+  };
+  attributes: {
+    name: Attribute.String;
+    description: Attribute.String;
+    value: Attribute.String;
+  };
+}
+
 export interface ComponentsReviews extends Schema.Component {
   collectionName: 'components_components_reviews';
   info: {
@@ -20,38 +46,12 @@ export interface ComponentsReviews extends Schema.Component {
   };
 }
 
-export interface ComponentsProductFeature extends Schema.Component {
-  collectionName: 'components_components_product_features';
-  info: {
-    displayName: 'productFeature';
-    description: '';
-  };
-  attributes: {
-    name: Attribute.String;
-    description: Attribute.String;
-    value: Attribute.String;
-  };
-}
-
-export interface ComponentsProductAttributes extends Schema.Component {
-  collectionName: 'components_components_product_attributes';
-  info: {
-    displayName: 'productAttributes';
-    description: '';
-  };
-  attributes: {
-    name: Attribute.String & Attribute.Required;
-    value: Attribute.String & Attribute.Required;
-    description: Attribute.Text & Attribute.Required;
-  };
-}
-
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
-      'components.reviews': ComponentsReviews;
-      'components.product-feature': ComponentsProductFeature;
       'components.product-attributes': ComponentsProductAttributes;
+      'components.product-feature': ComponentsProductFeature;
+      'components.reviews': ComponentsReviews;
     }
   }
 }
